aff7fa343b91df77a79ef5567bf9065461a6324d
jrodrigo1981 <jrodrigo1981@gmail.com>
2021-09-07 18:08:25 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://www.linkedin.com/in/rodrigo-gameiro" target="_blank" rel="noreferrer">Rodrigo Gameiro</a></p>
+                <p className="text-light"><small><strong>Spring React</strong><br />
+                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer> 
+
+    );
+}
+
+export default Footer;
